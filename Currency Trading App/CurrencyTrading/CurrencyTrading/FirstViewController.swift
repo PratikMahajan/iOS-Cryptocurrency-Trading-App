9e901ac9972e653c23a7ff50d0066d56301f112b
PratikMahajan <Pratik@mahajan.xyz>
2018-12-02 01:15:28 -0500

    initial commit

---
+++ Currency Trading App/CurrencyTrading/CurrencyTrading/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CurrencyTrading
+//
+//  Created by Pratik Mahajan on 12/2/18.
+//  Copyright © 2018 Pratik Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
